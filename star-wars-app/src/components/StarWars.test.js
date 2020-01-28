0a963003186c47c7c7a80d54b4660a5747bfb247
Dani Blackwell <bleuxgenius@gmail.com>
2020-01-28 17:23:16 -0600

    Update starwars.test.js
    MVP completed

--- star-wars-app/src/components/StarWars.test.js
+++ star-wars-app/src/components/StarWars.test.js
@@ -5,26 +5,37 @@ import {getData as mockGetData} from '../api'
 
 jest.mock('../api');
 
+// test for fake data
 const data = {
     next: 'placeholder',
     previous: null,
-    results: [{}]
+    results: [{
+        name: 'Luke',
+        height: '172',
+    },
+{
+   name: 'Dark Vader',
+   height: '85' 
+},
+{
+    name: 'Leai',
+    height: '60'
+}]
 }
 
-
 test('renders', async () => {
-    mockGetData.mockResolvedValue(); 
+    mockGetData.mockResolvedValue(data); 
     // AAA Arrange, Act, Assert
 
         const { getByText } = render(<StarWarsCharacters/>);
-
         const nextButton = getByText(/next/i);
         const previousButton = getByText(/previous/i);
 
         fireEvent.click(nextButton);
         fireEvent.click(previousButton);
 
-        
+        await wait(() => expect(getByText(/Luke/i)))
+        getByText("Luke");
 
-
+        expect(mockGetData).toHaveBeenCalledTimes(1);
 })
